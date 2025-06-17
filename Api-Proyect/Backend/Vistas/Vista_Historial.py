890ea54fe703c8fd1b5f08da74f5b66d8703cdcb
Sergio Pinzon <Sergio Pinzon>
2025-06-17 10:52:35 -0500

    Agregando correo

--- Api-Proyect/Backend/Vistas/Vista_Historial.py
+++ Api-Proyect/Backend/Vistas/Vista_Historial.py
@@ -20,7 +20,7 @@ class VistaHistorial(Resource):
             movimientos.append({
                 "id": venta.Id_Venta,
                 "fecha": venta.Fecha_Venta.isoformat() if venta.Fecha_Venta else None,
-                "usuario": venta.usuario.Nombre_Usu if venta.usuario else "Desconocido",
+                "usuario": venta.usuario.Email_Usu if venta.usuario else "Desconocido",
                 "total": float(venta.Total_Venta or 0),
                 "movimiento": venta.movimiento.value
             })
@@ -29,7 +29,7 @@ class VistaHistorial(Resource):
             movimientos.append({
                 "id": producto.Id_Producto,
                 "fecha": producto.Fecha_Registro_Prod.isoformat() if producto.Fecha_Registro_Prod else None,
-                "usuario": producto.usuario.Nombre_Usu if producto.usuario else "Desconocido",
+                "usuario": producto.usuario.Email_Usu if producto.usuario else "Desconocido",
                 "total": float(producto.Precio_Neto_Unidad_Prod or 0),
                 "movimiento": producto.movimiento.value
             })
